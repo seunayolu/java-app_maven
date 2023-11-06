532c4405681ad5695c460d355eeb6999314c7f72
Tolulope <omosefunmiblessing@gmail.com>
2023-11-06 09:27:13 -0700

    Update script.groovy

--- Jenkins_simple_pipeline/script.groovy
+++ Jenkins_simple_pipeline/script.groovy
@@ -3,10 +3,13 @@ def buildJar() {
     sh 'mvn package'
 } 
 
-def buildImage() {
+def buildImageDockerhub() {
     echo "building the docker image..."
-    echo "please build"
-} 
+    withCredentials([usernamePassword(credentialsId: '[ID_Docker_Repo]', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+        sh 'docker build -t [repository_name/repository_tag:imageID] .'
+        sh "echo $PASS | docker login -u $USER --password-stdin"
+        sh 'docker push [repository_name/repository_tag:imageID]'} 
+}
 
 def deployApp() {
     echo 'deploying the application...'
